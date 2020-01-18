b316f778aa7f66841a739c56d7635133cda71164
masonpiao <mhpark84@gmail.com>
2020-01-18 16:52:19 +0900

    0118

--- 0111_6.py
+++ 0111_6.py
@@ -4,7 +4,8 @@ print('곱셈 : *')
 print('나눗셈 : /')
 
 menu = input('메뉴 선택 >>')
-
+n1 = int(input('숫자1 입력 : '))
+n2 = int(input('숫자2 입력 : '))
 
 if menu == '+' :
     print('{} + {} = {}'.format(n1,n2,n1+n2))
@@ -15,9 +16,7 @@ elif menu == '*' :
 elif menu == '/' :
     print('{} / {} = {}'.format(n1,n2,n1/n2))
 else:
-    break #print('잘못된 연산자입니다.')
+    print('잘못된 연산자입니다.')
     
 
     
-n1 = int(input('숫자1 입력 : '))
-n2 = int(input('숫자2 입력 : '))
